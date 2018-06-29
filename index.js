9f5a6efda2476a01738934b0c59b72318ec54146
Jin Zhang <jnfzhang1995@gmail.com>
2018-06-29 08:11:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function sayHiToGrandma(string){
   if (string.toLowerCase() === string) 
     {return "I can't hear you!"} 
 }
+
 function sayHiToGrandma(string){
-  if ("I love you, Grandma" === string)
-  {return "I love you too"}
+  if (string.toUpperCase()===string)
+  {return "YES INDEED!"}
 }
