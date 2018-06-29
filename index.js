8d037d7381e83c499ce9f404346586d0a6275ae9
Jin Zhang <jnfzhang1995@gmail.com>
2018-06-29 08:17:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function logWhisper(string)
 {console.log(whisper(string))}
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string) 
-    {return "I can\t hear you!"} 
+    {return "I can't hear you!"} 
 }
